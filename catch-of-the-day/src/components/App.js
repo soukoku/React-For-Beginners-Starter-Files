8f498e0903ebf476b611d801170e596ffa7ea0eb
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2020-11-01 16:56:44 -0500

    Up to video 13

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Header from './Header'
+import Inventory from './Inventory'
+import Order from './Order'
+
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  }
+  addFish = fish => {
+    console.log('adding fish!')
+    const fishes = { ...this.state.fishes }
+    fishes[`fish${Date.now()}`] = fish
+    this.setState({
+      fishes: fishes
+    })
+  }
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          {/* <Fish /> */}
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { getFunName } from '../helpers'
+
+class StorePicker extends React.Component {
+  myInput = React.createRef()
+  goToStore = e => {
+    e.preventDefault()
+    const storeName = this.myInput.current.value
+    this.props.history.push(`/store/${storeName}`)
+  }
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          ref={this.myInput}
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        ></input>
+        <button type="submit">Visit Store</button>
+      </form>
+    )
+  }
+}
+
+export default StorePicker
